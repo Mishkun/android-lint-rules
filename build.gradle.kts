81fb1f0465e0e10a9cbd362d5328627821ee9f26
Mikhail <mishkun.lev@gmail.com>
2020-05-03 17:24:31 +0300

    Configure publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,12 @@ buildscript {
     repositories {
         google()
         jcenter()
+        maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.3")
+        classpath("gradle.plugin.com.github.b3er.local.properties:local-properties-plugin:1.1")
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     }
 }
 
--- nbsplint-publish/build.gradle.kts
+++ nbsplint-publish/build.gradle.kts
@@ -1,6 +1,84 @@
 plugins {
     id("com.android.library")
+    `maven-publish`
+    id("com.jfrog.bintray")
+    id("com.github.b3er.local.properties")
 }
+
+val pomName = "Android nbsp lint"
+val pomDesc = "Android lint rule to catch missing nbsp"
+val pomUrl = "https://github.com/Mishkun/android-lint-rules"
+val pomScmUrl = "https://github.com/Mishkun/android-lint-rules"
+val pomIssuesUrl = "https://github.com/Mishkun/android-lint-rules/issues"
+val pomDeveloperId = "mishkun"
+val pomDeveloperName = "Mikhail Levchenko"
+
+val pomLicenseName = "MIT"
+val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+
+val artifactVersion = "1.0.0"
+val group = "io.github.mishkun"
+val artifact = "nbsp-android-lint"
+
+operator fun <T> Property<T>.plusAssign(value: T) = set(value)
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("nbsp-lint") {
+            artifact("$buildDir/outputs/aar/nbsplint-publish-release.aar")
+
+            groupId = group
+            artifactId = artifact
+            version = artifactVersion
+
+            pom {
+                name += pomName
+                description += pomDesc
+                url += pomUrl
+                licenses {
+                    name += pomLicenseName
+                    url += pomLicenseUrl
+                }
+                developers {
+                    developer {
+                        id += pomDeveloperId
+                        name += pomDeveloperName
+                    }
+                }
+                issueManagement {
+                    url += pomIssuesUrl
+                }
+            }
+        }
+    }
+}
+
+configure<com.jfrog.bintray.gradle.BintrayExtension> {
+    user = project.findProperty("bintray.user").toString()
+    key = project.findProperty("bintray.apiKey").toString()
+    publish = true
+
+    setPublications("nbsp-lint")
+
+    pkg.apply {
+        repo = "mAndroidLint-rules"
+        name = "nbsp-lint"
+        user = "mishkun"
+        userOrg = pomDeveloperId
+        vcsUrl = pomScmUrl
+        description = pomDesc
+        setLicenses(pomLicenseName)
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssuesUrl
+
+        version.apply {
+            name = artifactVersion
+            desc = pomDesc
+            vcsTag = artifactVersion
+        }
+    }
+}
+
 
 android {
     compileSdkVersion(Sdk.COMPILE_SDK_VERSION)
